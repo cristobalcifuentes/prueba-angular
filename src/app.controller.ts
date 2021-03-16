f183f3d844338b3606834ea6c908ba481ded92ce
Marco Acevedo Espinoza <macves@macves.macves>
2021-03-16 09:23:05 -0300

    put y delete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 import { User } from './interfaces/user.interface';
 import { UserDto } from './dto/user.dto';
@@ -15,5 +15,15 @@ export class AppController {
   @Post('add')
   createUser(@Body() createUser: UserDto) : any {
     return {result: 'Ok', message: 'Usuario insertado con exito'};
+  }
+
+  @Put('update/:id')
+  update(@Param('id') id: string, @Body() updateUserDto: UserDto) {
+    return `This action updates a #${id} user, body: ${updateUserDto.username}`;
+  }
+
+  @Delete('delete/:id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id}`;
   }
 }
